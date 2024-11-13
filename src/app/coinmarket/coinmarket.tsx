7cd97cd5548731658de2d9b6420ed66556c33f45
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-11-13 22:18:58 +0500

    Add files via upload

--- src/app/coinmarket/coinmarket.tsx
+++ src/app/coinmarket/coinmarket.tsx
@@ -1,15 +1,33 @@
 "use client";
 import { useState, useEffect } from "react";
 
+interface Rates {
+  BTC: number;
+  BNB: number;
+  ETH: number;
+  LTC: number;
+  XRP: number;
+  DOGE: number;
+  USDT: number;
+  BCH: number;
+  XLM: number;
+  ADA: number;
+  SOL: number;
+}
+
+interface ApiResponse {
+  rates: Rates;
+}
+
 export default function CoinMarket() {
   const API_KEY = "e1300c22800059c0c920c87cf00bf51e";
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<ApiResponse | null>(null);
 
   useEffect(() => {
     fetch(`http://api.coinlayer.com/live?access_key=${API_KEY}`)
       .then((response) => response.json())
-      .then((jsonConverted) => {
-        console.log("JSON Converted Data : ", jsonConverted);
+      .then((jsonConverted: ApiResponse) => {
+        console.log("JSON Converted Data:", jsonConverted);
         setData(jsonConverted);
       });
   }, []);
